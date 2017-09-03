a89fdbd188979e46b1f2589f60c15e3dd62564af
shokry suleiman <shokrysuleiman032@gmail.com>
2017-09-03 18:41:16 +0200

    Adding  Animation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,31 @@
-import {Component, ViewChild} from '@angular/core';
+import { ViewChild} from '@angular/core';
 import {NavController, Slides} from 'ionic-angular';
+import { Component, trigger, transition, style, state, animate, keyframes } from '@angular/core';
 
 import { MainPage } from '../main/main';
 
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  animations: [
+
+    trigger('bounce', [
+      state('*', style({
+        transform: 'translateX(0)'
+      })),
+      transition('* => rightSwipe', animate('700ms ease-out', keyframes([
+        style({transform: 'translateX(0)', offset: 0}),
+        style({transform: 'translateX(-65px)',  offset: 0.3}),
+        style({transform: 'translateX(0)',     offset: 1.0})
+      ]))),
+      transition('* => leftSwipe', animate('700ms ease-out', keyframes([
+        style({transform: 'translateX(0)', offset: 0}),
+        style({transform: 'translateX(65px)',  offset: 0.3}),
+        style({transform: 'translateX(0)',     offset: 1.0})
+      ])))
+    ])
+  ]
 })
 export class HomePage {
 
@@ -24,6 +44,19 @@ export class HomePage {
   slideChanged() {
     if (this.slides.isEnd())
       this.skipMsg = "Alright, I got it";
+  }
+
+  state: string = 'x';
+
+  slideMoved() {
+    if (this.slides.getActiveIndex() >= this.slides.getPreviousIndex())
+      this.state = 'rightSwipe';
+    else
+      this.state = 'leftSwipe';
+  }
+
+  animationDone() {
+    this.state = 'x';
   }
 
 }
